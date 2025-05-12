a1ec594d88cd8c2c8f78853fed3f8046757caff1
plantpalfynn <plantpalfynn@gmail.com>
2025-05-12 10:06:56 +0200

    change syntax in registers

--- src/main/java/edu/kit/kastel/vads/compiler/backend/x84/x84Registers.java
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/x84/x84Registers.java
@@ -3,30 +3,28 @@ package edu.kit.kastel.vads.compiler.backend.x84;
 import edu.kit.kastel.vads.compiler.backend.regalloc.Register;
 
 public interface x84Registers extends Register {
-    public enum RealRegisters implements x84Registers
-    {
-            EAX,
-                    EBX,
-                    ECX,
-                    EDX,
-                    ESI,
-                    EDI,
-                    RSP,
-                    RBP,
-                    R8D,
-                    R9D,
-                    R10D,
-                    R11D,
-                    R12D,
-                    R13D,
-                    R14D,
-                    R15D;
+  public enum RealRegisters implements x84Registers {
+    EAX,
+    EBX,
+    ECX,
+    EDX,
+    ESI,
+    EDI,
+    RSP,
+    RBP,
+    R8D,
+    R9D,
+    R10D,
+    R11D,
+    R12D,
+    R13D,
+    R14D,
+    R15D;
 
-
-        @Override
-        public String toString() {
-            return "%" + name().toLowerCase();
-        }
+    @Override
+    public String toString() {
+      return name().toLowerCase();
     }
+  }
 
-    }
+}
